e143443303c22ba76d4cd29fd79cd8b99b6fd004
AnandJeyakumar <janand819@gmail.com>
2024-12-21 13:06:10 +0400

    Modifed Hidden dropdown

--- tests/HiddenDropdown.spec.js
+++ tests/HiddenDropdown.spec.js
@@ -1,6 +1,6 @@
 import {test,expect} from "@playwright/test"
 
-test("Hidden",async ({page})=>{
+test.skip("Hidden",async ({page})=>{
 
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
